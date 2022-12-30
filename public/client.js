976044c7d2d8689c35f643d747f3391240d393e6
ISHAAN SAHU <98735376+ishaan51551@users.noreply.github.com>
2022-12-30 20:12:41 +0530

    public/

---
+++ public/client.js
@@ -0,0 +1,56 @@
+const socket = io() //client lib
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter') {
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message){
+    let msg={
+        user:name,
+        message:message.trim()
+    }
+    //append
+    appendMessage(msg,'outgoing')
+    textarea.value=''
+    scrollBottom();
+
+    //send to server
+
+    socket.emit('message',msg); //emit for send lib
+
+}
+ 
+function appendMessage(msg,type){
+
+    let mainDiv=document.createElement('div')
+    let className=type
+    mainDiv.classList.add(className,'message')
+
+    let markUp=`
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML=markUp;
+
+    messageArea.appendChild(mainDiv)
+}
+
+//receive the msgs
+
+socket.on('message',(msg)=>{
+
+    appendMessage(msg,'incoming')
+    scrollBottom()
+})
+
+function scrollBottom(){
+    messageArea.scrollTop=messageArea.scrollHeight;
+}
